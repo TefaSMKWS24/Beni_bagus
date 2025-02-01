a689a6fc0c280990ac37c2ee3678e73517ac4a90
TefaSMKWS24 <tefaproject.smkws@gmail.com>
2025-02-01 11:32:41 -0800

    modified:   app/Http/Controllers/BarangController.php

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -4,22 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\redirect;
+use Illuminate\Support\Facades\validator;
+
 class BarangController extends Controller
 {
-    /**
+     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
-    }
 
+    public function index()
+
+    {
+        return view('barang.index');
+    }
     /**
      * Show the form for creating a new resource.
      */
     public function create()
     {
-        //
+        \\return view('barang.create');
     }
 
     /**
@@ -51,14 +56,35 @@ class BarangController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
-    }
+        $request->validate([
+            'name_barang' => 'required',
+            'harga' => 'required',
+            'stok' => 'required',
+            'kode_kategori' => 'required'
+        ]);
+
+        $data = [
+            'name_barang' => $request->name_barang,
+            'harga' => $request->harga,
+            'stok' => $request->stok,
+            'kode_kategori' => $request->kode_kategori
+        ];
+
+        DB::table('barang')->where('id', $id)->update($data);
+        return redicet()->view('barang.index', [
+
+        ]);
+
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
     {
-        //
-    }
-}
+        Db::table('barang')->where('id', $id)->delete();
+        return redirect()->route('barang.index');
+
+    };
+};
+
+
