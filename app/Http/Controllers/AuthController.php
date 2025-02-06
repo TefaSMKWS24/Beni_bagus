9800469558de7699735ffdd4758d963f2461d2a9
TefaSMKWS24 <tefaproject.smkws@gmail.com>
2025-02-06 09:23:20 -0800

    new file:   app/Http/Controllers/AuthController.php 	modified:   app/Http/Controllers/BarangController.php 	modified:   app/Http/Controllers/KasirController.php 	modified:   app/Http/Controllers/KategoriController.php 	modified:   app/Http/Controllers/PelangganController.php 	modified:   app/Http/Controllers/SupplierController.php 	modified:   app/Http/Controllers/TransaksiController.php

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function loginkasir(Request $request)
+    {
+        if(Auth::guard('kasir')->attempt([
+                                    'kode_kasir' => $request->nis,
+                                    'password' => $request->password]))
+        {
+            dd('Berhasil: '.Auth::guard('kasir')->user());
+            Log::info('Login successful');
+            //return redirect('/user/dashboard');
+        }
+        else{
+            echo "Login Gagal";
+            //return redirect('/user')->with('warning', 'NIS / Password Salah!');
+        }
+    }
+    public function logoutkasir()
+    {
+        if(Auth::guard('kasir')->check()){
+            Auth::guard('kasir')->logout();
+            return redirect('/');
+        }
+    }
+
+    public function loginadmin(Request $request)
+    {
+        if(Auth::guard('admin')->attempt([
+                                    'nik' => $request->nis,
+                                    'password' => $request->password]))
+        {
+            dd('Berhasil: '.Auth::guard('admin')->user());
+            Log::info('Login successful');
+            //return redirect('/user/dashboard');
+        }
+        else{
+            echo "Login Gagal";
+            //return redirect('/user')->with('warning', 'NIS / Password Salah!');
+        }
+    }
+    public function logoutadmin()
+    {
+        if(Auth::guard('admin')->check()){
+            Auth::guard('admin')->logout();
+            return redirect('/');
+        }
+    }
+}
