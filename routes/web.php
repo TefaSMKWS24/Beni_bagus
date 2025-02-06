f80eba102775b38274a403b5e8624ad6f6310602
TefaSMKWS24 <tefaproject.smkws@gmail.com>
2025-02-06 09:29:06 -0800

    modified:   app/Http/Middleware/Authenticate.php 	modified:   app/Http/Middleware/RedirectIfAuthenticated.php 	modified:   app/Models/data_kasir.php 	modified:   app/Providers/RouteServiceProvider.php 	new file:   resources/views/auth/login.blade.php 	new file:   resources/views/barang/create.blade.php 	new file:   resources/views/barang/edit.blade.php 	new file:   resources/views/barang/index.blade.php 	new file:   resources/views/kasir/create.blade.php 	new file:   resources/views/kasir/edit.blade.php 	new file:   resources/views/kasir/index.blade.php 	new file:   resources/views/kategori/create.blade.php 	new file:   resources/views/kategori/edit.blade.php 	new file:   resources/views/kategori/index.blade.php 	new file:   resources/views/pelanggan/create.blade.php 	new file:   resources/views/pelanggan/edit.blade.php 	new file:   resources/views/pelanggan/index.blade.php 	new file:   resources/views/supplier/index.blade.php 	new file:   resources/views/transaksi/create.blade.php 	new file:   resources/views/transaksi/index.blade.php 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\http\controllers\GuestController;
+use App\http\controllers\AuthController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\KasirController;
+use App\Http\Controllers\kategoriController;
+use App\Http\Controllers\TransaksiController;
+use App\Http\Controllers\PelangganController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +24,33 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+// GUEST (sebelum login)
+Route::middleware(['guest:kasir'])->group(function () {
+ Route::get('/kasir', function () {return view('auth.loginkaisir');})->name('loginkasir');
+ Route::post('/loginkasir', [AuthController::class, 'loginkasir']);
+});
+
+Route::middleware(['guest:admin'])->group(function () {
+ Route::get('/admin', function () {return view('auth.loginadmin');})->name('loginadmin');
+ Route::post('/loginadmin', [AuthController::class, 'loginadmin']);
+});
+
+// AUTH (setelah login)
+ Route::middleware(['auth:kasir'])->group(function () {
+    Route::get('/kasir/dashboard', [DashbordkasirController::class, 'dashboard']);
+    Route::get('/kasir/logout', [AuthController::class, 'logoutkasir']);
+});
+
+ Route::middleware(['auth:admin'])->group(function () {
+    Route::get('/admin/dashboard', [DashbordadminController::class, 'dashboard']);
+    Route::get('/admin/logout', [AuthController::class, 'logoutadmin']);
+
+    Route::resource('barang', BarangController::class);
+    Route::resource('kasir', KasirController::class);
+    Route::resource('kategori', kategoriController::class);
+    Route::resource('transaksi', TransaksiController::class);
+    Route::resource('pelanggan', PelangganController::class);
 });
