41aea329f35037fac9ec970f13dc274c7f815c87
TefaSMKWS24 <tefaproject.smkws@gmail.com>
2025-02-13 10:43:25 -0800

    modified:   resources/views/barang/index.blade.php

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -24,4 +24,33 @@
 
 @section('content')
 
+<table>
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Nama Barang</th>
+            <th>Harga</th>
+            <th>Stok</th>
+            <th>Kategori</th>
+            <th>Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($barang as $item)
+        <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $item->kode_barang }}</td>
+            <td>{{ $item->nama_barang }}</td>
+            <td>{{ $item->kategori->nama_kategori }}</td>
+            <td>{{ $item->harga }}</td>
+            <td>{{ $item->stok }}</td>
+            <td>
+                <a href="/barang/{{ $item->kode_barang }} }}/edit" class="btn btn-primary">Edit</a>
+                <form action="/barang/delete/{{ $item->kode_barang }}"  class="btn btn-danger">hapus</a>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
 @endsection
