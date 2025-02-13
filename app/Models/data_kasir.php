5757397a94276e8550470670d4db356a5e4eef77
Tefaproject <tefaproject.smk@gamil.com>
2025-02-14 06:03:45 +0700

    modified:   app/Models/data_kasir.php

--- app/Models/data_kasir.php
+++ app/Models/data_kasir.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class kasir extends Authenticatable
+class data_kasir extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
