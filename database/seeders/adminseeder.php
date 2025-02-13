290f1d4003761860b11e88868f17a318346bb365
TefaSMKWS24 <tefaproject.smkws@gmail.com>
2025-02-12 16:17:24 -0800

    modified:   database/seeders/adminseeder.php 	modified:   database/seeders/kasirseeder.php

--- database/seeders/adminseeder.php
+++ database/seeders/adminseeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class adminseeder extends Seeder
 {
@@ -16,7 +18,7 @@ class adminseeder extends Seeder
             [
                 'email' => 'admin@admin.com',
                 'name' => 'Admin',
-                'password' => hash::make('1234'),
+                'password' => hash::make('12345678S'),
 
             ],
         ];
